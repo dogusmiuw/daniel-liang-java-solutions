4beef4436cbb5d6a3ac0e9574128e1a66b547ab5
dous <dogukancelikcse@gmail.com>
2022-10-30 00:00:27 +0300

    exercise 1.9 done

---
+++ chapter-1/Exercise01_09.java
@@ -0,0 +1,7 @@
+class Exercise01_09 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+        System.out.println("Area = " + height*width);
+    }
+}

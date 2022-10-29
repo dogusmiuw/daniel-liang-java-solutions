5df0f996fbb39da654873080633687ab1345bed9
dous <dogukancelikcse@gmail.com>
2022-10-29 20:05:55 +0300

    file names changed

--- chapter-1/Exercise01_01.java
+++ chapter-1/Exercise01_01.java
@@ -1,4 +1,4 @@
-class Zero_One{
+class Exercise01_01{
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
--- chapter-1/Exercise01_02.java
+++ chapter-1/Exercise01_02.java
@@ -1,4 +1,4 @@
-class Zero_Two {
+class Exercise01_02 {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++){
             System.out.println("Welcome to Java");

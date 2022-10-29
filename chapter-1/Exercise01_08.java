59e322d45cda912c63f2bdeb3ab15cf4b4968b86
dous <dogukancelikcse@gmail.com>
2022-10-29 23:59:23 +0300

    exercise 1.8 done

---
+++ chapter-1/Exercise01_08.java
@@ -0,0 +1,9 @@
+class Exercise01_08 {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double perimeter = 2 * r * Math.PI;
+        double area = r * r * Math.PI;
+        System.out.println("Perimeter = " + perimeter); 
+        System.out.println("Area = " + area); 
+    }
+}
